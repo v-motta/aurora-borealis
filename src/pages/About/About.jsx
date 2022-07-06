4b544e83235b460bf115d2e3b85c520d3523abd6
Vinicius Motta <vinicius.costa@trescon.com.br>
2022-07-05 23:33:12 -0300

    adding navbar component

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
 import "./About.css";
 
 const About = () => {
     return (
         <>
+            <Navbar />
         </>
     )    
 }
