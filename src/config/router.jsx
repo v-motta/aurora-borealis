2943919342dc6c826acc48e6353cc2f3fb4bcc5b
Vinicius Motta <vinicius.costa@trescon.com.br>
2022-07-05 23:31:10 -0300

    adding routes

--- src/config/router.jsx
+++ src/config/router.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import About from "../pages/About/About";
+import Contact from "../pages/Contact/Contact";
 import Home from "../pages/Home/Home";
+import Projects from "../pages/Projects/Projects";
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="*" element={<Navigate to="/home" replace />} />
-                <Route path="/home" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/projects" element={<Projects />} />
             </Routes>
         </BrowserRouter>
     )    
