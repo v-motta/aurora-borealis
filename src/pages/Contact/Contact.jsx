4b0f67bbf4e57954f733223a7cc7338b5a9b30e4
Vinicius Motta <vinicius.costa@trescon.com.br>
2022-07-05 23:31:56 -0300

    new pages

---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import "./Contact.css";
+
+const Contact = () => {
+    return (
+        <>
+            <Navbar />
+        </>
+    )    
+}
+
+export default Contact;
---
+++ src/pages/Projects/Projects.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import "./Projects.css";
+
+const Projects = () => {
+    return (
+        <>
+            <Navbar />
+        </>
+    )    
+}
+
+export default Projects;
